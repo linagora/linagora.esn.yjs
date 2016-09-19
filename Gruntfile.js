0b7ac9b56d279b5a5e00a9a397ab8c6dd12f2939
Dung Dung T PHAM <dtpham@linagora.com>
2016-09-19 11:43:56 +0700

    MEET-537 Upgrade packages to newest version, fix minor test error.

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,7 +44,7 @@ module.exports = function(grunt) {
       options: {
         jshintrc: '.jshintrc',
         reporter: CI && 'checkstyle',
-        reporterOutput: CI && 'jshint.xml'
+        reporterOutput: CI ? CI : 'jshint.xml'
       },
       all: {
         src: [
